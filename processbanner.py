e6e1be125bddc838e740a1db6f41296a8f5bb4a8
@ChrisTruncer <ctruncer@christophertruncer.com>
2013-07-29 00:24:42 -0400

    Updated process whitelist

--- processbanner.py
+++ processbanner.py
@@ -4,84 +4,58 @@
 import os
 import re
 import socket
+import time
 
-network = '192.168.1.102'
+network = '10.250.250.250'
 port = 6667
 
-# Use the OS command to dump running processes minus those which are whitelisted within the command
-os.system('tasklist /fi \"IMAGENAME ne System Idle Process\" /fi \"IMAGENAME ne System\"  > processes.txt')
+while 1:
+	# Use the OS command to dump running processes minus those which are whitelisted within the command
+	os.system('tasklist /fi \"IMAGENAME ne System Idle Process\" /fi \"IMAGENAME ne System\" /fi \"IMAGENAME ne tasklist.exe\" /fi \"IMAGENAME ne dwm.exe\" /fi \"IMAGENAME ne explorer.exe\" /fi \"IMAGENAME ne svchost.exe\" /fi \"IMAGENAME ne dfssvc.exe\" /fi \"IMAGENAME ne vmtoolsd.exe\" /fi \"IMAGENAME ne smss.exe\" /fi \"IMAGENAME ne crss.exe\" /fi \"IMAGENAME ne winint.exe\" /fi \"IMAGENAME ne services.exe\" /fi \"IMAGENAME ne lsass.exe\" /fi \"IMAGENAME ne lsm.exe\" /fi \"IMAGENAME ne MsMpEng.exe\" /fi \"IMAGENAME ne SLsvc.exe\" /fi \"IMAGENAME ne spoolsv.exe\" /fi \"IMAGENAME ne dfsrs.exe\" /fi \"IMAGENAME ne dns.exe\" /fi \"IMAGENAME ne ismserv.exe\" /fi \"IMAGENAME ne processbanner.exe\" /fi \"IMAGENAME ne dfssvc.exe\" /fi \"IMAGENAME ne dllhost.exe\" /fi \"IMAGENAME ne msdtc.exe\" /fi \"IMAGENAME ne csrss.exe\" /fi \"IMAGENAME ne winlogon.exe\" /fi \"IMAGENAME ne taskeng.exe\" /fi \"IMAGENAME ne dwm.exe\" /fi \"IMAGENAME ne explorer.exe\" /fi \"IMAGENAME ne msseces.exe\" /fi \"IMAGENAME ne Oobe.exe\" /fi \"IMAGENAME ne mmc.exe\" /fi \"IMAGENAME ne taskmgr.exe\" /fi \"IMAGENAME ne cmd.exe\" /fi \"IMAGENAME ne TrustedInstaller.exe\" /fi \"IMAGENAME ne WmiPrvSE.exe\" /fi \"IMAGENAME ne wininit.exe\" /fi \"IMAGENAME ne logbanner.exe\" /fi \"IMAGENAME ne MSEdetector.exe\" /fi \"IMAGENAME ne MSASCui.exe\" /fi \"IMAGENAME ne wuauclt.exe\" /fi \"IMAGENAME ne SLUI.exe\" > processes.txt')
 
-# Open our files we will be reading from and writing to
-file = open('processes.txt','r')
-file2 = open('badprocesses.txt','w')
+	# Open our files we will be reading from and writing to
+	file = open('processes.txt','r')
+	file2 = open('badprocesses.txt','w')
 
-# Loop that "greps" for the IP address and writes it to a file
-for line in file:
+	# Loop that "greps" for the IP address and writes it to a file
+	for line in file:
 		line = line.split(" ")[0]
 		file2.write(line + '\n')
 
-file2.close()
-file2 = open('badprocesses.txt','r')
-processes = file2.readlines()
-file3 = open('sanitizedprocess.txt','w')
-file3.writelines(processes[4:])
+	file.close()
+	file2.close()
+	file3 = open('badprocesses.txt','r')
+	processes = file3.readlines()
+	file4 = open('sanitizedprocess.txt','w')
+	file4.writelines(processes[4:])
 
-file.close()
-file2.close()
-file3.close()
-
-os.system('del processes.txt')
-os.system('del badprocesses.txt')
-
-filesize = os.lstat("sanitizedprocess.txt")[6]
-
-if filesize == 0:
-	print "It's empty"
-else:
-	os.system('netstat -n > networkconns.txt')
-	file4 = open('networkconns.txt','r')
-	file5 = open('foreignips.txt','w')
-	nextline = file4.readlines()
-	file5.writelines(nextline[4:])
+	file3.close()
 	file4.close()
-	file5.close()
-	os.system('del networkconns.txt')
-	file6 = open('foreignips.txt','r')
-	file7 = open('ipport.txt','w')
-	for badips in file6:
-		badips = badips.split(" ")[9]
-		file7.write(badips + '\n')
-	file6.close()
-	file7.close()
-	file8 = open('ipport.txt','r')
-	file9 = open('badips.txt','w')
-	for ips in file8:
-		ips = ips.split(":")[0]
-		file9.write(ips + '\n')
-	file8.close()
-	file9.close()
-	targetfile = open('badips.txt','r')
-	readsorted = sorted(set(targetfile.readlines()))
-	targetfile.close()
-	#file10 = open('badips.txt','r')
-	for ban in readsorted:
-		ban = ban.strip()
-		command = "netsh advfirewall firewall add rule name=\"Banned " + ban + "\" action=block dir=in remoteip=" + ban
-		os.system(command)
-		irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-		irc.connect ( ( network, port ) )
-		irc.recv ( 4096 )
-		irc.send ( 'NICK tester2\r\n' )
-		irc.send ( 'USER tester2 completely real :Jxxx\r\n' )
-		irc.send ( 'JOIN #test\r\n' )
-		irc.send ( 'PRIVMSG #test :Blocked ' + ban + '\r\n' )
-		irc.send ( 'QUIT :Testing message\r\n' )
-		irc.close()
 
-#9 spaces
+	os.system('del processes.txt')
+	os.system('del badprocesses.txt')
 
-os.system('del sanitizedprocess.txt')
-os.system('del ipport.txt')
-os.system('del foreignips.txt')
-os.system('del badips.txt')
-print 'Done!'
+	filesize = os.lstat("sanitizedprocess.txt")[6]
+
+	if filesize == 0:
+		time.sleep(1)
+	else:
+		targetfile = open('sanitizedprocess.txt','r')
+		sortedprocesses = targetfile.readlines()
+		targetfile.close()
+		for process in sortedprocesses:
+			process = process.strip()
+			irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+			irc.connect ( ( network, port ) )
+			irc.send ( 'NICK pban1\r\n' )
+			data = irc.recv ( 4096 )
+			data = data.split(":")[1]
+			irc.send ( 'PONG ' + data + '\r\n' )
+			irc.send ( 'USER pban1 completely real :Jxxx\r\n' )
+			irc.send ( 'JOIN #arttreport\r\n' )
+			irc.send ( 'PRIVMSG #arttreport :Investigate ' + process + ' on me!\r\n' )
+			irc.send ( 'QUIT :Testing message\r\n' )
+			irc.close()
+
+	os.system('del sanitizedprocess.txt')
+	time.sleep(300)
